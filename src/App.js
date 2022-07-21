34f4d398fd0af943d2f91f4a8a1328b990e5b162
Arif11819 <93979453+Arif11819@users.noreply.github.com>
2022-07-21 12:07:58 +0600

    added all file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import Dashboard from './components/Dashboard/Dashboard';
-
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
 function App() {
   return (
     <div>
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './Dashboard.css';
 
 const Dashboard = () => {
     return (
-        <div>
-            <h1>Hello From Dashboard</h1>
+        <div className='dashboard-container'>
+            <h1 className='text-5xl text-center pt-10 pb-4 font-bold'>Dash-board</h1>
+            <div className='grid grid-cols-1 lg:grid-cols-3 md:grid-cols-3'>
+                <div data-aos="zoom-in" data-aos-duration="2500" className=' img-container'><img className='w-2/3' src="https://i.ibb.co/ZLkGKRN/Social-Dashboard-rafiki.png" alt="" /></div>
+                <div data-aos="zoom-in" data-aos-duration="2500" className=' img-container'><img className='w-2/3' src="https://i.ibb.co/xMcXfLd/Setup-Analytics-bro.png" alt="" /></div>
+                <div data-aos="zoom-in" data-aos-duration="2500" className=' img-container'><img className='w-2/3' src="https://i.ibb.co/dJm3X4m/Dashboard-bro.png" alt="" /></div>
+            </div>
         </div>
     );
 };
