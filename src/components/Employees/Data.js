048b2a6db2316817701dd2290c4cfe85ef1445c9
Md Mazharul Islam <mazharulislam.dev@gmail.com>
2022-07-21 21:38:25 +0600

    employees design added

---
+++ src/components/Employees/Data.js
@@ -0,0 +1,58 @@
+
+
+export const employees = [
+    {
+        img: "https://i.ibb.co/LSQtR9Q/istockphoto-1317804578-612x612.jpg",
+        comment: 'Very user-friendly and customizable.',
+        name: 'jeni',
+        position: 'Accounting'
+    },
+    {
+        img: "https://i.ibb.co/7SJbgGP/company-idea-im01.jpg",
+        comment: "HR's best friend and so relative",
+        name: 'CASSIE F.',
+        position: 'Nonprofit'
+    },
+    {
+        img: "https://i.ibb.co/gz8nJ36/istockphoto-1338134319-612x612.jpg",
+        comment: 'Smart, lean and seamless solution.',
+        name: 'JASON J.',
+        position: 'IT Consulting'
+    },
+    {
+        img: "https://i.ibb.co/wdBDpDr/images.jpg",
+        comment: 'The onboarding and service is exceptional.',
+        name: 'DAWN G.',
+        position: 'Marketing'
+    },
+    {
+        img: "https://i.ibb.co/yqjN3hq/linkedin-profile-photography.jpg",
+        comment: 'Our employees seem to love it!',
+        name: 'JENNIFER B.',
+        position: 'Engineering'
+    },
+    {
+        img: "https://i.ibb.co/gVMC9dk/images-1.jpg",
+        comment: 'Only perf mgnt tool I have enjoyed using!',
+        name: 'CIARA F.',
+        position: 'Software'
+    },
+    {
+        img: "https://i.ibb.co/hM6rx7t/istockphoto-1307791650-612x612.jpg",
+        comment: "He's Saved us so much time.",
+        name: 'CARLA M.',
+        position: 'Packaging Solutions'
+    },
+    {
+        img: "https://i.ibb.co/ydzwbqz/img02.jpg",
+        comment: 'Flexible, easy and intuitive work.',
+        name: 'ASHLEY A.',
+        position: 'Consulting'
+    },
+    {
+        img: "https://i.ibb.co/kg8bDSV/President-021.jpg",
+        comment: 'Awesome way to track goals!',
+        name: 'JAN K.',
+        position: 'Engineering'
+    },
+]
