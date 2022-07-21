89942bb14b779795853cc0898d49a709c929ba32
Arif11819 <93979453+Arif11819@users.noreply.github.com>
2022-07-21 14:24:16 +0600

    added all

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-
-
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import Header from './components/Header/Header';
+import Dashboard from './components/Dashboard/Dashboard';
+import Features from './components/Feature/Features/Features';
+import Footer from './components/Footer/Footer';
+AOS.init();
 
 function App() {
   return (
     <div >
-     
+      <Header></Header>
+      <Dashboard></Dashboard>
+      <Features></Features>
+      <Footer></Footer>
     </div>
   );
 }
