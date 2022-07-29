c4d57a475cb8aeae1125b9944a391065c98a3c6a
Md Mazharul Islam <mazharulislam.dev@gmail.com>
2022-07-29 14:15:28 +0600

    git pull from development

--- src/components/Feature copy/SingleFeatures/SingleFeatures.js
+++ src/components/Feature copy/SingleFeatures/SingleFeatures.js
@@ -1,26 +1,5 @@
 import React from 'react';
 
-<<<<<<< HEAD:src/components/Feature copy/SingleFeatures/SingleFeatures.js
-
-const SingleFeatures = ({feature}) => {
-  const {name, url, description} = feature;
-  return (
-    <div data-aos="flip-left" data-aos-duration="1500">
-      <div className="card lg:w-72 lg:h-42 bg-slate-200 shadow-xl">
-          <figure className="px-20 pt-10">
-            <img
-              src={url}
-              alt="Features Pictures"
-              className="rounded-xl"
-            />
-          </figure>
-          <div className="card-body items-center text-center">
-            <h2 className="card-title font-bold text-orange-400 text-2xl">{name}</h2>
-            <p>{description}</p>
-            
-          </div>
-        </div> 
-=======
 const SingleFeatures = ({ feature }) => {
   const { name, url, description } = feature;
   return (
@@ -38,7 +17,6 @@ const SingleFeatures = ({ feature }) => {
         <p>{description}</p>
 
       </div>
->>>>>>> eeffd72d4b38f79ab19557840a477f51d791b4f8:src/components/Feature/SingleFeatures/SingleFeatures.js
     </div>
 
   );
