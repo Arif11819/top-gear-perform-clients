14e9f3686632791a5d8efd38b97b21e9737a6db9
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2022-07-31 21:54:12 +0600

    frist-commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import './App.css';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import Header from './components/Header/Header';
-import Dashboard from './components/Dashboard/Dashboard';
-import Features from './components/Feature/Features/Features';
-import Footer from './components/Footer/Footer';
-import Logo from './components/Logo/Logo';
-AOS.init();
+
 
 function App() {
   return (
     <div >
-      <Header></Header>
-      <Dashboard></Dashboard>
-      <Features></Features>
-      <Logo/>
-      <Footer></Footer>
+      
     </div>
   );
 }
