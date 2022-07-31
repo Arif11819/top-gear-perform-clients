37e8a40d59920e0b48c6d76ee1cf33e074aec1e0
unknown <fboy4069@gmail.com>
2022-07-31 20:17:23 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
-import Faq from './Components/FAQ/Faq';
-import Footer from './Components/Footer/Footer';
+
 
 
 function App() {
   return (
 
     <div>
-      <Faq />
-      <Footer />
+
     </div>
 
   );
