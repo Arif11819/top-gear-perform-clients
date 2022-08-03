e8cdac093b1c0e30649938060edfa5d1bb4bfa50
Arif11819 <93979453+Arif11819@users.noreply.github.com>
2022-08-03 22:35:13 +0600

    added file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 import Home from './components/Home/Home';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init();
 
 
 function App() {
---
+++ src/components/Home/CustomerCaseStudy/CustomerCaseStudy.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CustomerCaseStudy.css';
+
+const CustomerCaseStudy = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default CustomerCaseStudy;
