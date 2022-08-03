1525db9f64fc97834d790558a64a1f4a132aad93
unknown <fboy4069@gmail.com>
2022-08-04 00:26:17 +0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import './App.css';
+import Motive from './components/Motive/Motive';
 
 
-
-
-
-
-import Header from './components/Home/Header/Header';
-import { Route, Routes } from 'react-router-dom';
-import Login from './components/pages/Login/Login';
-import Navbar from './components/Home/Header/Navbar';
 
 
 
@@ -16,13 +9,9 @@ import Navbar from './components/Home/Header/Navbar';
 function App() {
   return (
 
-    <>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Header />} />
-        <Route path='/login' element={<Login />} />
-      </Routes>
-    </>
+    <div>
+      <Motive />
+    </div>
   )
 
 
