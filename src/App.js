3909eb59d8d0a0d40e157c96e247a93178e86ea2
Torikul Islam <toriqulislam142@gmail.com>
2022-08-03 11:36:49 -0400

    added login and signup route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Header from './components/Home/Header/Header';
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import Register from './components/pages/SignUp/Register';
+import { Route, Routes } from 'react-router-dom';
+import Login from './components/pages/Login/Login';
+import Navbar from './components/Home/Header/Navbar';
 
 AOS.init()
 
@@ -11,7 +14,11 @@ function App() {
   return (
 
     <>
-    <Header />
+    <Navbar />
+   <Routes>
+    <Route path='/' element={<Header />} />
+    <Route path='/login' element={<Login />} />
+   </Routes>
     </>
 
   );
--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css'
-const Login = () => {
+import { useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init'
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+const Login = ({ setModal }) => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [loginError, setLoginError] = useState('')
+    const navigate = useNavigate()
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    if (loading) {
+        console.log(loading)
+    }
+    if (user) {
+        console.log(user)
+        navigate('/')
+        setModal(false)
+    }
+    const handleLogin = (event) => {
+        event.preventDefault()
+
+        signInWithEmailAndPassword(email, password)
+
+    }
+
+
+
     return (
-        <div>
+        <div className='login-container'>
             
+            <form onSubmit={handleLogin} className='login-form'>
+                <input autoComplete='off' onBlur={(e) => setEmail(e.target.value)} type="text" placeholder='Email' name='email' required />
+                <input autoComplete='off' onBlur={(e) => setPassword(e.target.value)} type="password" placeholder='Password' name='password' required />
+                <div className='register'>
+                </div>
+                <button type='submit'>Login</button>
+            </form>
+
+            <div className="right-side">
+
+                side bar
+            </div>
+
+
         </div>
     );
 };
