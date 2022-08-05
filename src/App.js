ab35a7544357fca9a055e749e15d8ff87cb70017
Torikul Islam <toriqulislam142@gmail.com>
2022-08-05 05:52:16 -0400

    login page modified

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from './components/Home/Header/Header';
 import { Route, Routes } from 'react-router-dom';
 import Login from './components/pages/Login/Login';
 import Navbar from './components/Home/Header/Navbar';
+import Register from './components/pages/SignUp/Register'
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
@@ -16,6 +18,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Header />} />
         <Route path='/login' element={<Login />} />
+        <Route path= '/signUp' element = {<Register />} />
       </Routes>
     </>
   )
