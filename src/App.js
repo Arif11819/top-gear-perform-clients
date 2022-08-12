06001ee274584df132a1327118b125e1c6817daa
sheikhsumaya <sheikhsumaya622@gmail.com>
2022-08-12 15:17:35 +0600

    terms

--- src/App.js
+++ src/App.js
@@ -24,6 +24,8 @@ import Chat from './components/pages/Dashboard/Chat/Chat';
 import Notifications from './components/pages/Dashboard/Notifications/Notifications';
 import Action from './components/pages/Dashboard/Actions/Action';
 import ScheduleSubmit from './components/Home/ScheduleDemo/ScheduleSubmit';
+import Team from './components/Home/Team/Team';
+import Terms from './components/pages/Login/Terms';
 
 
 
@@ -53,6 +55,8 @@ function App() {
         <Route path='/scheduleSubmit' element={<ScheduleSubmit />} />
         <Route path='/helpCenter' element={<HelpCenter />} />
         <Route path='/ebook' element={<Ebooks />} />
+        <Route path='/team' element={<Team/>} />
+        <Route path='/terms' element={<Terms/>} />
         <Route path='/faq' element={<Faq />} />
         <Route path='/reviews/:reviewsId' element={<FullReview />} />
         <Route path='*' element={<NotFound />}></Route>
