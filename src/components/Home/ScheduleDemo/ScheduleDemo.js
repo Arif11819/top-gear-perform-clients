6035518a35f922cc1d87be3ad5869ecc84a509ec
Md Mazharul Islam <mazharulislam.dev@gmail.com>
2022-08-15 13:03:44 +0600

    timeSlots fetch from database

--- src/components/Home/ScheduleDemo/ScheduleDemo.js
+++ src/components/Home/ScheduleDemo/ScheduleDemo.js
@@ -22,16 +22,18 @@ const ScheduleDemo = () => {
     const [email, setEmail] = useState('');
     const [companyName, setCompanyName] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
+    const [bookedTime, setBookedTime] = useState('');
     useEffect(() => {
-        fetch("timeSlots.json")
+        fetch("http://localhost:5000/timeSlots")
             .then(res => res.json())
             .then(data => setTimeSlots(data))
     }, [])
+    const formattedDate = format(date, 'PP');
     const handleSubmit = () => {
         const scheduleData = {
             timeZone: timeZone,
             language: languageSelect,
-            date: date,
+            date: formattedDate,
             time: time,
             userName: userName,
             email: email,
@@ -49,10 +51,10 @@ const ScheduleDemo = () => {
             .then(res => res.json())
             .then(data => {
                 if (data.success === true) {
-                    console.log(data);
                     navigate('/scheduleSubmit')
                 } else {
-                    console.log("already book this time");
+                    setBookedTime('Your selected time is already booked')
+
                 }
             })
     }
@@ -69,7 +71,7 @@ const ScheduleDemo = () => {
                 </div>
                 <p className='ml-3 my-4 text-slate-500'>Date</p>
                 <div className='p-4'>
-                    <ScheduleCalender date={date} setDate={setDate} time={time} setTime={setTime} timeSlots={timeSlots} setTimeSlots={setTimeSlots} />
+                    <ScheduleCalender bookedTime={bookedTime} date={date} setDate={setDate} time={time} setTime={setTime} timeSlots={timeSlots} setTimeSlots={setTimeSlots} />
                     <p className='bg-[#cbe2f7] p-3 font-bold text-sm'>Selected Date: {format(date, 'PPPP')} {time}</p>
                 </div>
                 <div className='flex justify-center'>
@@ -82,7 +84,7 @@ const ScheduleDemo = () => {
                     <button>Submit</button>
                 </div>
                 <div className='text-center pb-4'>
-                    <p>By submitting this form, I agree to Profit.co's <a className='text-[#1fade4]' href="#">Privacy Policy.</a> </p>
+                    <p>By submitting this form, I agree to topgearperform's <a className='text-[#1fade4]' href="#">Privacy Policy.</a> </p>
                 </div>
             </div>
             <div className='text-center py-8 bg-[#f5f8fd] mt-5'>
