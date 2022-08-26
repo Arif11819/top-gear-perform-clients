1696b47713d7d930cc2aebc9ef1ea96be8630999
unknown <fboy4069@gmail.com>
2022-08-26 15:44:47 +0600

    deploy

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -51,7 +51,7 @@ const Chart = ({ ch }) => {
                     <h1 className='flex items-center gap-1'><BsPeople></BsPeople><p className='text-sm'>Reprots to {reportsto}</p></h1>
                     <h1 className='flex items-center gap-1'><RiOrganizationChart></RiOrganizationChart><p className='text-sm'>{reports} directs reports</p></h1>
                 </div>
-                <button onClick={() => handleDelete(ch._id)} className='btn btn-primary btn-md'>Remove User</button>
+                <button onClick={() => handleDelete(ch._id)} className='text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-4 py-3 text-center'>Remove User</button>
             </div>
 
         </div>
--- src/components/Home/Header/Navbar.js
+++ src/components/Home/Header/Navbar.js
@@ -60,7 +60,7 @@ const Navbar = () => {
                             <label tabIndex="0">CUSTOMERS<RiArrowDownSLine className='arrow-icon' /> </label>
                             <ul tabIndex="0" className="dropdown-content menu  shadow bg-base-100 w-72">
                                 <li><Link to='/dashboard/reviews'><img src="https://i.ibb.co/TMJZqKB/employee-engagemnet.png" alt="" /> Case Study</Link></li>
-                                <li><Link to='/dashboard/course'><img src="https://i.ibb.co/thRDcVS/integrations.png" alt="" /> Why TopGear Perform</Link></li>
+                                <li><Link to='/praise'><img src="https://i.ibb.co/thRDcVS/integrations.png" alt="" /> Why TopGear Perform</Link></li>
                             </ul>
                         </div>
 
